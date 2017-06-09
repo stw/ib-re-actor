896cf4ddeca8979fec19204c4bcaa32f8d6331dc
Steve Walker <swalker@walkertek.com>
2017-06-09 13:41:47 -0400

    update to deploy to clojars

--- project.clj
+++ project.clj
@@ -1,11 +1,16 @@
-(defproject ib-re-actor "0.1.1-SNAPSHOT"
+(defproject org.clojars.stw/ib-re-actor "0.1.1-SNAPSHOT"
   :description "Clojure friendly wrapper for InteractiveBrokers java API"
+  :url "https://github.com/stw/ib-re-actor"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["clojars" {:url "https://clojars.org/repo"
+                             :creds :gpg}]]  
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.4"]
-                 [com.ib/jtsclient "9.68"]
+                 [org.clojars.stw/twsapi "9.72.18"]
+;;                 [com.ib/jtsclient "9.68"]
                  [org.clojure/tools.logging "0.2.3"]
                  [clj-logging-config "1.9.10"]]
-  :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :plugins [[lein-midje "2.0.1"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]
                                   [com.stuartsierra/lazytest "1.2.3"]]}})
